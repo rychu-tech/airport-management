725fd5e735bdf9069ee8b7b54906ac11f9d9383d
rychu-tech <rychudotka14@gmail.com>
2023-09-28 22:22:16 +0200

    Added helper class that is simply the condition checker and exception thrower for carrier endpoints. Added edit endpoint and get list of carriers.

--- project/src/main/java/com/airport/manager/project/features/carrier/services/CarrierService.java
+++ project/src/main/java/com/airport/manager/project/features/carrier/services/CarrierService.java
@@ -2,23 +2,29 @@ package com.airport.manager.project.features.carrier.services;
 
 import com.airport.manager.project.features.carrier.exceptions.CarrierNameExistsException;
 import com.airport.manager.project.features.carrier.exceptions.CarrierNotFoundException;
+import com.airport.manager.project.features.carrier.helpers.CarrierChecker;
 import com.airport.manager.project.features.carrier.models.Carrier;
 import com.airport.manager.project.features.carrier.repositories.CarrierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CarrierService {
     private CarrierRepository carrierRepository;
+    private CarrierChecker carrierChecker;
     @Autowired
-    public CarrierService(CarrierRepository carrierRepository) {
+    public CarrierService(
+            CarrierRepository carrierRepository,
+            CarrierChecker carrierChecker
+    )
+    {
         this.carrierRepository = carrierRepository;
+        this.carrierChecker = carrierChecker;
     }
     public Carrier addCarrier(Carrier carrier) throws CarrierNameExistsException {
-        Carrier carrierLookup = carrierRepository.findByName(carrier.getName());
-        if (carrierLookup != null) {
-            throw new CarrierNameExistsException();
-        }
+        carrierChecker.checkCarrierName(carrier.getName());
         return carrierRepository.save(carrier);
     }
 
@@ -27,13 +33,19 @@ public class CarrierService {
     }
 
     public Carrier deleteCarrierById(Long carrierId) {
-        Carrier carrier = this.findById(carrierId);
-
-        if (carrier == null) {
-            throw new CarrierNotFoundException();
-        }
-
+        Carrier carrier = carrierChecker.checkCarrierId(carrierId);
         carrier.setActive(false);
+        return carrierRepository.save(carrier);
+    }
+
+    public List<Carrier> findAll() {
+        return carrierRepository.findAll();
+    }
+
+    public Carrier editCarrier(Long carrierId, Carrier carrierData) {
+        carrierChecker.checkCarrierName(carrierId, carrierData.getName());
+        Carrier carrier = carrierChecker.checkCarrierId(carrierId);
+        carrier.setName(carrierData.getName());
         return carrierRepository.save(carrier);
     }
 }
