0c5c4cd76eb5239eaec5f353f928cea4b4c66495
rychu-tech <rychudotka14@gmail.com>
2023-09-29 21:39:58 +0200

    Added transactional to component dataseeder

--- project/src/main/java/com/airport/manager/project/DataSeeder.java
+++ project/src/main/java/com/airport/manager/project/DataSeeder.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Component
 public class DataSeeder {
     private final AirplaneStatusRepository airplaneStatusRepository;
